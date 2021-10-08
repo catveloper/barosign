09ed4aae7842f9abd8e35f031adc6f6ec3f21b63
donguk.im <donfuk.im@datamaker.io>
2021-10-08 10:43:56 +0900

    update urlpatterns

--- apps/calculator/urls.py
+++ apps/calculator/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from apps.calculator.views import FreightFV
+from apps.calculator.views import *
 
 app_name = 'calculator'
 
 urlpatterns = [
-    path('freight/', FreightFV.as_view(), name='freight'),
+    path('freight/', FreightFV.as_view(), name='freight_fv'),
+    path('freights/', FreightLV.as_view(), name='freight_lv'),
 ]
