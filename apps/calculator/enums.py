c3490abe4a32ea37cd9d2b0450b4efce362e34e4
donguk.im <donfuk.im@datamaker.io>
2021-10-14 11:07:36 +0900

    Add enum -> dictionary function

--- apps/calculator/enums.py
+++ apps/calculator/enums.py
@@ -1,4 +1,6 @@
+import copy
 from enum import Enum, EnumMeta
+from typing import Any
 
 from django.db.models.enums import ChoicesMeta
 
@@ -9,12 +11,17 @@ class CustomEnumMeta(EnumMeta):
     def choices(cls):
         return [(instance.name, instance.value[0]) for instance in cls]
 
+    def to_dict(cls):
+        result: dict = {}
+        for instance in cls:
+            instance_info: dict = copy.deepcopy(instance.__dict__)
+            del instance_info['__objclass__']
+            result[instance.name] = instance_info
+        return result
+
 
 class CustomEnum(Enum, metaclass=CustomEnumMeta):
-
-    @classmethod
-    def to_dict(cls):
-        return {instance.name: instance for instance in cls}
+    pass
 
 
 class ChargeType(CustomEnum):
@@ -26,3 +33,14 @@ class ChargeType(CustomEnum):
         self.unit_html = unit_html
 
 
+class LorryType(CustomEnum):
+    SMALL = ('작은차', 1740, 1970, 1000, 300)
+    MEDIUM = ('중간차', 2460, 2670, 5000, 500)
+    LARGE = ('큰차', 3000, 3000, 10000, 700)
+
+    def __init__(self, kr_name, width, height, loadable_weight, loadable_volume):
+        self.kr_name = kr_name
+        self.width = width
+        self.height = height
+        self.loadable_weight = loadable_weight
+        self.loadable_volume = loadable_volume
