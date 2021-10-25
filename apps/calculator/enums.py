9006331509b601eaf3329bb6c7da1748df5ac971
donguk.im <donfuk.im@datamaker.io>
2021-10-25 15:30:56 +0900

    화물차 타입 추가

--- apps/calculator/enums.py
+++ apps/calculator/enums.py
@@ -34,13 +34,24 @@ class ChargeType(CustomEnum):
 
 
 class LorryType(CustomEnum):
-    SMALL = ('작은차', 1740, 1970, 1000, 300)
-    MEDIUM = ('중간차', 2460, 2670, 5000, 500)
-    LARGE = ('큰차', 3000, 3000, 10000, 700)
+    ONE_TONE = ('1톤', 1500, 2800, 800, 1000)
+    ONE_HALF_TONE = ('1.4톤', 1550, 3100, 800, 1500)
+    TWO_HALF_TONE = ('2.5톤', 1900, 4300, 1030, 25000)
+    THREE_HALF_TONE = ('3.5톤', 2090, 4500, 1050, 3500)
+    FIVE_TONE = ('4.5톤-5톤', 2280, 6250, 1100, 5000)
+    FIVE_TONE_PLUS = ('5톤 플러스', 2300, 7250, 1200, 5000)
+    SIX_HALF_TONE = ('6.5톤 플러스축', 2350, 7400, 1200, 6500)
+    ELEVEN_TONE = ('11톤', 2350, 9100, 1400, 11000)
+    FOURTEEN_TONE = ('14톤', 2350, 9300, 1400, 14000)
+    EIGHTEEN_TONE = ('18톤', 2350, 10100, 1400, 18000)
+    TWENTY_FIVE_TONE = ('25톤', 2350, 10100, 1400, 25000)
+    TRAILER = ('트레일러', 2500, 12000, 1600, 30000)
+    LOW_BED_TRAILER = ('로베드', 2500, 5600, 800, 30000)
 
-    def __init__(self, kr_name, width, height, loadable_weight, loadable_volume):
+    def __init__(self, kr_name, width,length, height, loadable_weight):
         self.kr_name = kr_name
         self.width = width
+        self.length = length
         self.height = height
         self.loadable_weight = loadable_weight
-        self.loadable_volume = loadable_volume
+
